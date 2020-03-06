30e4e7b3fc02f565b79e37e1b994487a6543d664
Patrick Musau <pmusau13ster@gmail.com>
2020-03-06 17:17:29 -0600

    Build the offboard node

--- run_docker.sh
+++ run_docker.sh
@@ -6,6 +6,7 @@ docker run -it --rm --runtime=nvidia --privileged --env="QT_X11_NO_MITSHM=1" \
     --env=LOCAL_USER_ID="$(id -u)" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $(pwd)/src/:/home/src:rw \
+    -v $(pwd)/catkin_ws/:/home/catkin_ws:rw \
     -e DISPLAY=:0 \
     -p 14570:14570/udp \
     --name=px4_containter px4 bash
